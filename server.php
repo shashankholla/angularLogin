cd1ab7e822a3db3e5605cffec4d4b3bf1f2a5299
shashankholla <shashu1999@gmail.com>
2019-02-18 19:54:52 +0530

    Server.php changes

--- server.php
+++ server.php
@@ -4,12 +4,13 @@
 
 	session_start();
 	
-	
+	$namePOST = "";
 	$username = "";
 	$password = "";
-	$email    = "";
+	$emailPOST    = "";
 	$usernamePOST = "";
 	$passwordPOST = "";
+	$cpasswordPOST = "";
 	$_SESSION['success'] = "";
 	$errors = array(); 
 	$postdata = file_get_contents("php://input");
@@ -18,11 +19,17 @@
   
   $request = json_decode( $postdata, true);
 	
-	$usernamePOST = $request["data"][0]["username"];
-	$passwordPOST = $request["data"][0]["password"];
+	$whatThis = $request["data"]["process"];
+	
+	$usernamePOST = $request["data"]["username"];
+	
+	
 	
 }
 $db = mysqli_connect('localhost', 'root', 'shashank', 'registration');
+	
+if($whatThis == "login"){
+		$passwordPOST = $request["data"]["password"];
 		$username = mysqli_real_escape_string($db, $usernamePOST);
 		$password = mysqli_real_escape_string($db, $passwordPOST);
 		
@@ -40,7 +47,17 @@ $db = mysqli_connect('localhost', 'root', 'shashank', 'registration');
 			$results = mysqli_query($db, $query);
 
 			if (mysqli_num_rows($results) == 1) {
-				echo "YES";
+				$row = mysqli_fetch_array($results);
+				$op_obj = (object) [
+					'valid' => 'YES',
+					'username' => $row['username'],
+					'name' => $row['name']
+				];
+
+				echo json_encode($op_obj);
+
+		
+				
 				$_SESSION['username'] = $username;
 				
 			}else {
@@ -48,6 +65,63 @@ $db = mysqli_connect('localhost', 'root', 'shashank', 'registration');
 				array_push($errors, "Wrong username/password combination");
 			}
 		}
+	}
+
+	if($whatThis == "register")
+	
+	{	
+		$passwordPOST = $request["data"]["password"];
+		$namePOST = $request["data"]["name"];
+		$emailIdPOST = $request["data"]["emailId"];
+		$cpasswordPOST = $request["data"]["cpassword"];
+		if($cpasswordPOST == $passwordPOST)
+		{
+			$name = mysqli_real_escape_string($db, $namePOST);
+			$username = mysqli_real_escape_string($db, $usernamePOST);
+			$password = mysqli_real_escape_string($db, $passwordPOST);
+			$cpassword = mysqli_real_escape_string($db, $cpasswordPOST);
+			$emailId = mysqli_real_escape_string($db, $emailIdPOST);
+			$query = "SELECT * FROM users WHERE username='$username'";
+			$results = mysqli_query($db, $query);
+			if(mysqli_num_rows($results) == 0)
+			{	$md5_password = md5($password);
+				$query = "INSERT INTO users (name, username, email, password) 
+					  VALUES('$name','$username', '$emailId', '$md5_password')";
+				mysqli_query($db, $query);
+				echo "Account Created";
+			}
+			else{
+				echo "Username Exists";
+			}
+
+		}
+		else{
+			echo "Passwords dont match";
+		}
+	}
+
+	if($whatThis == "data")
+	{	
+		$username = mysqli_real_escape_string($db, $usernamePOST);
+		$query = "SELECT * FROM data WHERE username='$username'";
+		$results = mysqli_query($db, $query);
+		$row = mysqli_fetch_array($results);
+
+		$op_obj = (object) [
+			'farmId' => $row['farmId'],
+			'temp' => $row['temp'],
+			'humidity' => $row['humidity'],
+			'soilMoisture' => $row['soilMoisture'],
+			'waterLevel' => $row['waterLevel'],
+			'dateTime' => $row['dateTime']
+		];
+
+		echo json_encode($op_obj);
+
+
+
+
+	}
 	
 
 ?>
